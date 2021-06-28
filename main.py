df9563226bccd16d0cd09602517e760390cc0f44
maajtga <77799136+maajtga@users.noreply.github.com>
2021-06-28 10:47:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ Player1 = paddle.Paddle(70, 225)
 Player2 = paddle.Paddle(800, 225)
 Ball = ball.Ball(270, 400)
 
-ballspeedx = 0.1
-ballspeedy = 0.1
+ballspeedx = 0.5
+ballspeedy = 0.5
 
 while running:
     
@@ -28,15 +28,40 @@ while running:
     Player1.render(win)
     Player2.render(win)
     Ball.render(win)
-
+    
     Ball.x += ballspeedx
     Ball.y += ballspeedy
 
+    if Ball.x > 486 or Ball.x < 0:
+        ballspeedx *= -1
+
+    if Ball.y > 836 or Ball.y < 0:
+        ballspeedy *= -1
+
     for event in pygame.event.get():
+        
         if event.type == pygame.QUIT:
             running = False
 
         if event.type == pygame.KEYDOWN:
-            pass
+            
+            if event.key == pygame.K_w:
+                Player1.y -= 20
+
+            if event.key == pygame.K_s:
+                Player1.y += 20
+
+            if event.key == pygame.K_UP:
+                Player2.y -= 20
+
+            if event.key == pygame.K_DOWN:
+                Player2.y += 20
+
+        
+        if event.type == pygame.KEYUP:
+
+            if event.key == pygame.K_w:
+                Player1.y -= 20
+
 
     pygame.display.flip()
